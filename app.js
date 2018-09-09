0d814d68cb2278b8dfc327466b85d1969927bf8b
Ftujio <svetlin954@abv.bg>
2018-09-09 21:26:48 +0300

    Connecting to mongodb database using "mongoose"

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const PORT = 3000;
+const databaseConfig = require('./config/database');
+
+mongoose.connect(databaseConfig.connectionString, { useNewUrlParser: true });
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to database ${databaseConfig.connectionString}`);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(`Database error: ${err}`);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
